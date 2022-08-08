ec13ae86f31136370d2d98d3d4de25b9fe0e5025
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-08-08 15:55:52 +0300

    cannot be supported mac filterin on egress hook

--- src/netfilter/hooks.c
+++ src/netfilter/hooks.c
@@ -7,15 +7,14 @@
 #include "policy_table.h"
 #include "logger.h"
 
-const struct ethhdr* eth;
-const struct iphdr* iph;
-const struct udphdr* udp;
-const struct tcphdr* tcp;
-const struct net_device* dev;
-const policy_t* pol;
-
 
 unsigned int ip_ingress_hook(void* priv, struct sk_buff* skb, const struct nf_hook_state* state) {
+    const struct ethhdr* eth;
+    const struct iphdr* iph;
+    const struct udphdr* udp;
+    const struct tcphdr* tcp;
+    const struct net_device* dev;
+    const policy_t* pol;
     u32 sip;
     u16 sport=0, dport=0;
     u8 sha[ETH_ALEN], tha[ETH_ALEN];
@@ -64,44 +63,44 @@ unsigned int ip_ingress_hook(void* priv, struct sk_buff* skb, const struct nf_ho
 }
 
 unsigned int ip_egress_hook(void* priv, struct sk_buff* skb, const struct nf_hook_state* state) {
+    const struct iphdr* iph;
+    const struct udphdr* udp;
+    const struct tcphdr* tcp;
+    const struct net_device* dev;
+    const policy_t* pol;
     u32 dip;
     u16 sport=0, dport=0;
-    u8 sha[ETH_ALEN], tha[ETH_ALEN];
 
     if (unlikely(!skb))
         return NF_DROP;
 
     dev = skb->dev;
 
-    eth = eth_hdr(skb);
-    memcpy(sha, eth->h_source, ETH_ALEN);
-    memcpy(tha, eth->h_dest, ETH_ALEN);
-
     iph = ip_hdr(skb);
     dip = ntohl(iph->daddr);
     switch (iph->protocol) {
         case IPPROTO_ICMP:
-            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, tha, "icmp", 0, dip, 0, 0, 0);
+            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, NULL, "icmp", 0, dip, 0, 0, 0);
             break;
         case IPPROTO_UDP:
             udp = udp_hdr(skb);
             sport = ntohs(udp->source);
             dport = ntohs(udp->dest);
 
-            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, tha, "udp", 0, dip, sport, 0, 0);
+            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, NULL, "udp", 0, dip, sport, 0, 0);
             break;
         case IPPROTO_TCP:
             tcp = tcp_hdr(skb);
             sport = ntohs(tcp->source);
             dport = ntohs(tcp->dest);
 
-            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, tha, "tcp", 0, dip, sport, 0, 0);
+            pol = find_policy(0, OUTPUT, NULL, dev->name, NULL, NULL, "tcp", 0, dip, sport, 0, 0);
             break;
     }
 
     if (pol) {
         if (pol->target == DROP) {
-            log_info(OUTPUT, NULL, dev->name, sha, tha, iph->protocol, ntohs(iph->tot_len),
+            log_info(OUTPUT, NULL, dev->name, "", "", iph->protocol, ntohs(iph->tot_len),
                      iph->tos, iph->ttl, iph->saddr, iph->daddr, sport, dport);
             return NF_DROP;
         } else
