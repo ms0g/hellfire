4d04dc7d28bca2bb8117c558097e7aeaaa157b9d
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-08-31 02:34:36 +0300

    build(cli): replaced build type with release

--- build/builder.sh
+++ build/builder.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p build_temp && cd build_temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../.. && cmake --build .
+mkdir -p build_temp && cd build_temp || exit
+cmake -DCMAKE_BUILD_TYPE=Release ../.. && cmake --build .
